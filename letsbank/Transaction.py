379067928dd330fac82ebfde58d3b347d3262813
michal <michal>
2003-09-27 06:29:46 +0000

    added posted, note

--- letsbank/Transaction.py
+++ letsbank/Transaction.py
@@ -1,10 +1,12 @@
 
 from letsbank import Account
 from strongbox import *
-from pytypes import FixedPoint
+from pytypes import FixedPoint, Date
 
 class Transaction(Strongbox):
     amount = attr(FixedPoint)
+    posted = attr(Date, default="now")
+    note = attr(str, okay=lambda s: len(s) <= 255)
     src = link(Account)
     dst = link(Account)
 
--- letsbank/spec/TransactionTest.py
+++ letsbank/spec/TransactionTest.py
@@ -1,3 +1,14 @@
 
-class TransactionTest(object):
-    pass
+import unittest
+from letsbank import Transaction
+
+class TransactionTest(unittest.TestCase):
+
+    def test_note(self):
+        # note must be < 255 characters:
+        Transaction(note="x" * 255)
+        self.assertRaises(ValueError, Transaction, note="x" * 256)
+
+
+if __name__=="__main__":
+    unittest.main()
