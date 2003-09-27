c3a948736a149511a9e62369a38979d819664833
michal <michal>
2003-09-27 07:13:57 +0000

    added BankingApp, fixed field names

--- letsbank/__init__.py
+++ letsbank/__init__.py
@@ -2,12 +2,13 @@ from Account import Account
 from Transaction import Transaction
 from Bank import Bank
 from AccountAuth import AccountAuth
+from BankingApp import BankingApp
 
 DBMAP = {
     Account: "bank_account",    
     Transaction: "bank_transaction",    
-    Transaction.__attrs__['dst']: (Account, "dstID"),
-    Transaction.__attrs__['src']: (Account, "srcID"),
+    Transaction.__attrs__['dst']: (Account, "destID"),
+    Transaction.__attrs__['src']: (Account, "sourceID"),
     }
 
 
