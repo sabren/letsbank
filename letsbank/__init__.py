8ecb1f2380f4e0b5fa48e33ec93dd26430cefd93
michal <michal>
2003-09-27 05:04:15 +0000

    added DBMAP

--- letsbank/__init__.py
+++ letsbank/__init__.py
@@ -1,3 +1,13 @@
 from Account import Account
 from Transaction import Transaction
 from Bank import Bank
+from AccountAuth import AccountAuth
+
+DBMAP = {
+    Account: "bank_account",    
+    Transaction: "bank_transaction",    
+    Transaction.__attrs__['dst']: (Account, "dstID"),
+    Transaction.__attrs__['src']: (Account, "srcID"),
+    }
+
+
