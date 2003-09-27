0e3a2eb9b2a07185524ca4f9f259a3829274d741
michal <michal>
2003-09-27 05:04:02 +0000

    added randpass, contact fields

--- letsbank/Account.py
+++ letsbank/Account.py
@@ -1,10 +1,21 @@
 
 from strongbox import *
 from pytypes import FixedPoint
+from handy import randpass
+
+                       
 
 class Account(Strongbox):
     ID = attr(long)
+    fname = attr(str)
+    lname = attr(str)
+    email = attr(str)
     username = attr(str)
     password = attr(str)
     balance = attr(FixedPoint, default=0)
 
+    def __init__(self, **args):
+        super(Account, self).__init__(**args)
+        if not self.password:
+            self.password = randpass()
+            
