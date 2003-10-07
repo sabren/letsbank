e570733f9c159508c458a5c0a0524c16fcebc148
michal <michal>
2003-10-07 02:34:44 +0000

    calculate balance, history on fly

--- letsbank/Bank.py
+++ letsbank/Bank.py
@@ -1,4 +1,5 @@
 
+from pytypes import FixedPoint
 from letsbank import Account
 from letsbank import Transaction
 
@@ -16,23 +17,43 @@ class Bank(object):
             raise KeyError(username)
 
     def balanceFor(self, username):
-        a = self.getAccount(username)
-        return a.balance
+        balance = 0 
+        for item in self.historyFor(username):
+            balance += item["change"]            
+        return balance
 
 
     def historyFor(self, username):
         # @TODO: THIS SUCKS!! NEED TO ADD QUERIES TO ARLO
-        a =self.getAccount(username)
+        a = self.getAccount(username)
         history = [t for t in self.clerk.match(Transaction)
                    if (t.src.ID == a.ID) or (t.dst.ID == a.ID)]
         history.sort(lambda a,b: cmp(a.posted, b.posted))
-        return history
-        
+        res = []
+        for t in history:
+            change = t.amount
+            if t.src.ID == a.ID:
+                change *= -1
+                other = t.dst.username
+            else:
+                other = t.src.username
+            res.append({"posted":t.posted,
+                        "change":change,
+                        "other":other,
+                        "note":t.note,})
+        return res
+    
+    
         
     ## transfer management:
 
     def transfer(self, source, dest, amount, note):
-        if amount <= 0: raise ValueError("amount must be postive")
+        try:
+            amount = FixedPoint(amount)
+        except:
+            raise ValueError("badamt")
+        if amount <= 0: raise ValueError("nonpos")
+        if source==dest: raise ValueError("xferself")
         try:
             s = self.getAccount(source)
             d = self.getAccount(dest)
