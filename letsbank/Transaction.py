82cca7f243dffae2e094f18950a480060e8ee71a
michal <michal>
2003-10-07 02:36:31 +0000

    Date -> DateTime

--- letsbank/Transaction.py
+++ letsbank/Transaction.py
@@ -1,11 +1,11 @@
 
 from letsbank import Account
 from strongbox import *
-from pytypes import FixedPoint, Date
+from pytypes import FixedPoint, DateTime
 
 class Transaction(Strongbox):
     amount = attr(FixedPoint)
-    posted = attr(Date, default="now")
+    posted = attr(DateTime, default="now")
     note = attr(str, okay=lambda s: len(s) <= 255)
     src = link(Account)
     dst = link(Account)
